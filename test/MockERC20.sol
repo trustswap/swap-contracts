5221b147c53b1e8af51743bd6288112b47dccd88
Levike34 <l_meadows34@hotmail.com>
2024-02-16 16:39:08 +0900

    update

--- test/MockERC20.sol
+++ test/MockERC20.sol
@@ -1,20 +1,46 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.6.2 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
 
-import {ERC20} from "./ERC20.sol";
+contract MockERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
-contract MockERC20 is ERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Mint(address indexed to, uint256 value);
+
     constructor(
         string memory _name,
         string memory _symbol,
-        uint8 _decimals
-    ) ERC20(_name, _symbol, _decimals) {}
-
-    function mint(address to, uint256 value) public virtual {
-        _mint(to, value);
+        uint8 _decimals,
+        uint256 _initialSupply
+    ) public {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply * 10**uint256(_decimals);
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function burn(address from, uint256 value) public virtual {
-        _burn(from, value);
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        require(_to != address(0), "Invalid address");
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function mint(address _to, uint256 _value) public {
+        require(_to != address(0), "Invalid address");
+        
+        totalSupply += _value;
+        balanceOf[_to] += _value;
+        emit Mint(_to, _value);
+        emit Transfer(address(0), _to, _value);
     }
 }
--- test/SwapStakingContract.t.sol
+++ test/SwapStakingContract.t.sol
@@ -9,20 +9,22 @@ import {MockERC20} from "./MockERC20.sol";
 // import {TransparentUpgradeableProxy} from "../lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 
-
 contract SwapStakingContractTest is Test {
  
     uint256 bscFork;
 
     address upgradedImplementationAddress;
-    // MockERC20 erc20;
+    MockERC20 swap;
+    MockERC20 newSwapToken;
     SwapStakingContract ltsp;  
     SwapStakingContract ltspUpgrade;        
-      
+
+
     address user1 = address(0xdead);
     address user2 = address(0xbabe);
 
     address internal constant LTSP_PROXY = address(0x1714FBCFb62A4974C83EaFA0fAEC12191da6c71e);
+    address internal constant LTSP_PROXY_ADMIN_MANAGER = address(0xeB8ad7b4Eaa21562009b86F66Eea2894e6890A82);
     address internal constant LTSP_PROXY_ADMIN = address(0x77e8bC029D7b8738C0d3dA6d42955f64a0C70a26);
     address internal constant SWAP_BSC = address(0x82443A77684A7Da92FdCB639c8d2Bd068a596245);
     address internal constant USER_WITH_LOCKS = address(0xc02B86B768301DE377b37572c0fB2629c99b080a);
@@ -32,15 +34,20 @@ contract SwapStakingContractTest is Test {
         bscFork = vm.createFork("https://bsc-dataseed.binance.org/");
         ltsp = SwapStakingContract(LTSP_PROXY);
         vm.selectFork(bscFork);
-        // newSwapToken = new MockERC20("NEWSWAP", "NEWSWAP", 18);
+        newSwapToken = new MockERC20("NEWSWAP", "NEWSWAP", 18, UINT256_MAX);
         ltspUpgrade = new SwapStakingContract();
         upgradedImplementationAddress = address(ltspUpgrade);
 
-        // proxyAdmin = ProxyAdmin(LTSP_PROXY_ADMIN);
 
-        // swap = MockERC20(SWAP_BSC);
+        swap = MockERC20(SWAP_BSC);
     }
 
+    function test_1() public {
+        vm.startPrank(LTSP_PROXY_ADMIN);
+        bytes memory upgradeCallData = "0x99a88ec40000000000000000000000001714fbcfb62a4974c83eafa0faec12191da6c71e0000000000000000000000002e234dae75c793f67a35089c9d99245e1c58470b";
+        (bool success, bytes memory returnData) = address(LTSP_PROXY_ADMIN_MANAGER).call(upgradeCallData);
+        console.log(success);
 
+    }
 
 }
