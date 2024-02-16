0dbfe4f899684e619ca9ae234b4cb79a5033d365
murtrax <hahmed.bscs16seecs@seecs.edu.pk>
2024-02-16 09:10:46 +0500

    setup foundary

---
+++ test/SwapStakingContract.t.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.6.2;
+pragma experimental ABIEncoderV2;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract SwapStakingContractTest is Test {
+    function setup() public {
+        console.log("hello");
+    }
+
+    function test1() public {
+        console.log("yo");
+    }
+}
